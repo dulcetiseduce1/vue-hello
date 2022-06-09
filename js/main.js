40b2a913a07eb3bc22456e6e78c34bf9e4d4954e
dulcetiseduce1 <mariusdulce@tuta.io>
2022-06-09 15:44:40 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+const Ciao = new Vue({
+    // elemento html
+    el: "#app",
+    data: {
+      // messaggio
+      message: "un messaggio all'interno di un h1,utilizzando i data.",
+    },
+  
+    // Conterrà una serie di funzioni
+    methods: {
+      // funzioni che useremo nell'app
+    },
+  });
